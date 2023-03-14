cf1ed374607dbd003e88122da95dfafecf19f1a4
Kolawole Akinola <101843900+Royalwole@users.noreply.github.com>
2023-03-14 03:19:19 +0100

    ARGUMENTS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log("No argument");
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log("Best Argument found");
 } else {
-  console.log("Arguments found");
+  console.log("Best School Arguments found");
 }

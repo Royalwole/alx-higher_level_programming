70bf7ffcea98512a98720ef097e59ea9fc8d3642
Kolawole Akinola <101843900+Royalwole@users.noreply.github.com>
2023-03-14 21:20:01 +0100

    sorted js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
-// Script imports a dictionary of occurrences by user id
-// and computes a dictionary of user ids by occurrence.
 
-const { dict } = require('./101-data.js');
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
+const dict = require('./101-data.js'); // import the dictionary from the file
+
+const newDict = {}; // initialize the new dictionary
+
+// loop through the keys of the original dictionary
+for (const userId in dict) {
+  const occurrences = dict[userId]; // get the number of occurrences for this user
+
+  // check if the occurrences are already a key in the new dictionary
+  if (newDict[occurrences]) {
+    // if yes, add this user to the list of user ids for that occurrence count
+    newDict[occurrences].push(userId);
+  } else {
+    // if not, create a new key in the new dictionary with this occurrence count and add this user to the list of user ids
+    newDict[occurrences] = [userId];
+  }
 }
-console.log(Dictn);
+
+console.log(newDict); // print the new dictionary
+
